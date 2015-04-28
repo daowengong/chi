32b21c096551cae39d21229dd9e1616e2139e371
Administrator <Administrator@5TL84EEHA656RQH>
2015-04-29 01:07:28 +0800

    update

--- shoppingcart/src/test/java/shoppingcart/Test.java
+++ shoppingcart/src/test/java/shoppingcart/Test.java
@@ -3,6 +3,8 @@ package shoppingcart;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,74 +19,158 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		// Thumbnails.of("C:\\Users\\Administrator\\Desktop\\GONGDAOWEN\\0.png").size(200,
 		// 300).toFile("C:\\Users\\Administrator\\Desktop\\0_1.png");
-		DefaultConverter.register(".pdf", new PdfConverter());
-		DefaultConverter.register(".tif", new TiffConverter());
 		
 //		AutoConverter.newInstance().convert("C:\\Users\\Administrator\\Desktop\\test.pdf", null);
-		DefaultConverter.newInstance().convert("C:\\Users\\Administrator\\Desktop\\test.tif", "C:/test/");
+		FileConverter.autoConvert("C:\\Users\\Administrator\\Desktop\\test.tif", "C:/test/");
 	}
 
-	interface FileConverter {
-		void convert(String file1, String file2);
-	}
-
-	static class DefaultConverter implements FileConverter {
-		private static final DefaultConverter autoConverter = new DefaultConverter();
-		
-		static final Map<String, FileConverter> converterMap = new HashMap<String, FileConverter>();
+	static abstract class FileConverter {
+		/**
+		 *  不区分大小写键的Map
+		 */
+		static final Map<String, FileConverter> converterMap = new HashMap<String, FileConverter>(){
+			private static final long serialVersionUID = 1L;
+			public FileConverter put(String key, FileConverter value) {
+				return super.put(key != null ? key.toLowerCase() : null, value);
+			};
+			public FileConverter get(Object key) {
+				return super.get(key != null ? key.toString().toLowerCase() : null);
+			};
+		};
+		/**
+		 *  默认注册pdf和tif转换器
+		 */
 		static {
-			register(".gif", new PdfConverter());
+			register(".pdf", new PdfConverter());
+			register(".tif", new TiffConverter());
 		}
-		
-		public static DefaultConverter newInstance(){
-			return autoConverter;
+		/**
+		 *  注册转换器
+		 */
+		public static void register(String name, FileConverter converter) {
+			converterMap.put(name, converter);
 		}
-
-		public static void register(String regexp, FileConverter converter) {
-			converterMap.put(regexp, converter);
+		/**
+		 *  获取转换器
+		 */
+		public static FileConverter getConverter(String name) {
+			return converterMap.get(name);
 		}
-
-		public void convert(String file1, String file2) {
-			String name = new File(file1).getName();
-			if(name.contains("."))
+		/**
+		 *  转换文件
+		 */
+		public static void autoConvert(String srcFile, String destFile) {
+			String name = new File(srcFile).getName();
+			if(name.contains(".")) {
 				name = name.substring(name.lastIndexOf("."));
-			converterMap.get(name).convert(file1, file2);
+			}
+			// 根据后缀获取转换器
+			FileConverter fileConverter = converterMap.get(name);
+			if(fileConverter == null) { // 默认转换器
+				fileConverter = new DefaultConverter();
+			}
+			fileConverter.convert(srcFile, destFile);
 		}
 		
+		public static void convert(String srcFile, String destFile, String name) {
+			FileConverter fileConverter = converterMap.get(name);
+			if(fileConverter == null) {
+				throw new RuntimeException("don't support " + name + " convert.");
+			}
+			fileConverter.convert(srcFile, destFile);
+		}
+		
+		public static void convert(String srcFile, String destFile, Class<? extends FileConverter> converterClz) {
+			FileConverter fileConverter = null;
+			try {
+				fileConverter = converterClz.newInstance();
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+			fileConverter.convert(srcFile, destFile);
+		}
+		
+		public void convert(String srcFile, String destFile) {
+			try {
+				// 检查目录是否存在
+				checkDir(destFile);
+				// 获取图像
+				List<BufferedImage> images = getBufferedImages(new File(srcFile));
+				// 输出图像到指定位置
+				BufferedImage image = null;
+				for (int i = 0, le = images.size(); i < le; i++) {
+					// 压缩图像
+					image = compress(images.get(i));
+					// 输出图像
+					ImageIO.write(image, "jpg", new File(destFile + i + ".jpg"));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public abstract List<BufferedImage> getBufferedImages(File srcFile);
+		
+		/**
+		 * 检查目录，不存在则创建
+		 */
 		public void checkDir(String _dir){
 			File dir = new File(_dir);
 			if(!dir.exists()){
 				dir.mkdirs();
 			}
 		}
+		
+		/**
+		 * 压缩图像
+		 */
+		public BufferedImage compress(BufferedImage image){
+			return image;
+		} 
+	}
+	
+	static class DefaultConverter extends FileConverter {
+		@Override
+		public List<BufferedImage> getBufferedImages(File srcFile) {
+			List<BufferedImage> _images = new ArrayList<BufferedImage>();
+			try {
+				_images = Arrays.asList(Imaging.getBufferedImage(srcFile));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return _images;
+		}
+		
 	}
 
-	static class PdfConverter extends DefaultConverter {
-		public void convert(String pdfFile, String pdfDir) {
+	static class PdfConverter extends FileConverter {
+		@Override
+		public List<BufferedImage> getBufferedImages(File pdfFile) {
+			List<BufferedImage> _images = new ArrayList<BufferedImage>();
 			try {
-				checkDir(pdfDir);
 				List<?> pages = PDDocument.load(pdfFile).getDocumentCatalog().getAllPages();
 				for (int i = 0, le = pages.size(); i < le; i++) {
 					PDPage page = (PDPage) pages.get(i);
-					ImageIO.write(page.convertToImage(), "jpg", new File(pdfDir + i + ".jpg"));
+					_images.add(page.convertToImage());
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			return _images;
 		}
 	}
 
-	static class TiffConverter extends DefaultConverter {
-		public void convert(String tiffFile, String tiffDir) {
+	static class TiffConverter extends FileConverter {
+		@Override
+		public List<BufferedImage> getBufferedImages(File tiffFile) {
+			List<BufferedImage> _images = new ArrayList<BufferedImage>();
 			try {
-				checkDir(tiffDir);
-				List<BufferedImage> images = Imaging.getAllBufferedImages(new File(tiffFile));
-				for(int i = 0, le = images.size(); i < le; i ++){
-					ImageIO.write(images.get(i), "jpg", new File(tiffDir + i + ".jpg"));
-				}
+				_images = Imaging.getAllBufferedImages(tiffFile);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			return _images;
 		}
 	}
 }
